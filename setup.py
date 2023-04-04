1107e5153b5dd82692fb2d126bd7a9ab673ef673
Adam Goldstein <129613567+AdamGoldstein-USRA@users.noreply.github.com>
2023-04-04 12:32:51 -0500

    Merge conflict resolution

--- setup.py
+++ setup.py
@@ -1,7 +1,38 @@
-from setuptools import setup, find_packages
-import src.gdt.core as gdt_c
+# CONTAINS TECHNICAL DATA/COMPUTER SOFTWARE DELIVERED TO THE U.S. GOVERNMENT WITH UNLIMITED RIGHTS
+#
+# Contract No.: CA 80MSFC17M0022
+# Contractor Name: Universities Space Research Association
+# Contractor Address: 7178 Columbia Gateway Drive, Columbia, MD 21046
+#
+# Copyright 2017-2022 by Universities Space Research Association (USRA). All rights reserved.
+#
+# Developed by: William Cleveland and Adam Goldstein
+#               Universities Space Research Association
+#               Science and Technology Institute
+#               https://sti.usra.edu
+#
+# Developed by: Daniel Kocevski
+#               National Aeronautics and Space Administration (NASA)
+#               Marshall Space Flight Center
+#               Astrophysics Branch (ST-12)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing permissions and limitations under the
+# License.
+#
+import sys
+from setuptools import setup
+
+sys.path.append('src')
 
 if __name__ == '__main__':
+<<<<<<< HEAD
     setup(
         name='gdt-core',
         version=gdt_c.__version__,
@@ -41,4 +72,12 @@ if __name__ == '__main__':
     )
     # Create the GDT data directory, if it doesn't exist
     gdt_c.data_path.mkdir(parents=True, exist_ok=True)
+=======
+    import gdt.core as core
+>>>>>>> ec114d3 (moved the package details to pyproject.toml)
 
+    # For backward compatibility
+    setup()
+
+    # create library data directory
+    core.data_path.mkdir(parents=True, exist_ok=True)
--- src/gdt/core/__init__.py
+++ src/gdt/core/__init__.py
@@ -1,7 +1,35 @@
-__version__ = '2.0.0'
-
+# CONTAINS TECHNICAL DATA/COMPUTER SOFTWARE DELIVERED TO THE U.S. GOVERNMENT WITH UNLIMITED RIGHTS
+#
+# Contract No.: CA 80MSFC17M0022
+# Contractor Name: Universities Space Research Association
+# Contractor Address: 7178 Columbia Gateway Drive, Columbia, MD 21046
+#
+# Copyright 2017-2022 by Universities Space Research Association (USRA). All rights reserved.
+#
+# Developed by: William Cleveland and Adam Goldstein
+#               Universities Space Research Association
+#               Science and Technology Institute
+#               https://sti.usra.edu
+#
+# Developed by: Daniel Kocevski
+#               National Aeronautics and Space Administration (NASA)
+#               Marshall Space Flight Center
+#               Astrophysics Branch (ST-12)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing permissions and limitations under the
+# License.
+#
 import os
 from pathlib import Path
+
+__version__ = '2.0.0'
 
 suite_path = Path(__file__).parent.parent
 
@@ -11,8 +39,6 @@ else:
     base_path = Path.home().joinpath('.gammaray_data_tools', __version__)
 
 cache_path = base_path.joinpath('cache')
-# test_data = {instrument: test_data_path.joinpath(instrument) \
-#              for instrument in os.listdir(test_data_path)}
 
 if 'GDT_DATA' in os.environ:
     data_path = Path(os.environ['GDT_DATA'])
