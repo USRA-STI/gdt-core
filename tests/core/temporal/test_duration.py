b3ca2f52aa3b80bb065b1a27e0eb95bd7922af2e
Adam Goldstein <129613567+AdamGoldstein-USRA@users.noreply.github.com>
2024-02-23 16:22:36 -0600

    License headers

--- tests/core/temporal/test_duration.py
+++ tests/core/temporal/test_duration.py
@@ -1,24 +1,30 @@
+# CONTAINS TECHNICAL DATA/COMPUTER SOFTWARE DELIVERED TO THE U.S. GOVERNMENT WITH UNLIMITED RIGHTS
 #
-#     Authors: Oliver Roberts (USRA)
+# Contract No.: CA 80MSFC17M0022
+# Contractor Name: Universities Space Research Association
+# Contractor Address: 425 3rd Street SW, Suite 950, Washington, DC 20024
 #
-#     Portions of the code are Copyright 2020 William Cleveland and
-#     Adam Goldstein, Universities Space Research Association
-#     All rights reserved.
+# Copyright 2017-2024 by Universities Space Research Association (USRA). All rights reserved.
 #
-#     Written for the Fermi Gamma-ray Burst Monitor (Fermi-GBM)
+# Developed by: William Cleveland, Adam Goldstein and Oliver Roberts
+#               Universities Space Research Association
+#               Science and Technology Institute
+#               https://sti.usra.edu
 #
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
+# Developed by: Daniel Kocevski
+#               National Aeronautics and Space Administration (NASA)
+#               Marshall Space Flight Center
+#               Astrophysics Branch (ST-12)
 #
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
 #
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing permissions and limitations under the
+# License.
 #
 import numpy as np
 import os
