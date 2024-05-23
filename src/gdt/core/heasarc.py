0f4833e661b7db3d65821dae3e2a1d8d4900862c
Josh Wood <joshua.r.wood@nasa.gov>
2024-05-22 23:45:39 -0500

    filled in BaseFinder, FtpFinder. Restored FileDownloader, BrowseCatalog

--- src/gdt/core/heasarc.py
+++ src/gdt/core/heasarc.py
@@ -51,8 +51,7 @@ from rich.progress import (
     TransferSpeedColumn,
 )
 
-#__all__ = ['FtpFinder', 'BrowseCatalog', 'Http', 'Ftp', 'FileDownloader']
-__all__ = ['ProgressMixin', 'Ftp', 'Http']
+__all__ = ['ProgressMixin', 'Ftp', 'Http', 'BaseFinder', 'FtpFinder', 'FileDownloader', 'BrowseCatalog']
 
 
 class ProgressMixin:
@@ -68,6 +67,7 @@ class ProgressMixin:
             TransferSpeedColumn(), "•",
             TimeRemainingColumn(),
         )
+
 
 class BaseProtocol(AbstractContextManager, ABC, ProgressMixin):
     """A base class for the protocol used to access remote files.
@@ -224,6 +224,7 @@ class BaseProtocol(AbstractContextManager, ABC, ProgressMixin):
         """
         pass
 
+
 class Ftp(BaseProtocol):
     """A base class for FTP interactions with the HEASARC archive.
     
@@ -367,6 +368,7 @@ class Ftp(BaseProtocol):
     def __repr__(self):
         return '<{0}: host {1}>'.format(self.__class__.__name__, self._host)
 
+
 class Http(BaseProtocol):
     """A base class for HTTP/HTTPS interactions with the HEASARC archive.
     
@@ -375,7 +377,7 @@ class Http(BaseProtocol):
         url (str, optional): The url of the HTTP/HTTPS archive
     """
 
-    def __init__(self, *args, url='https://heasarc.gsfc.nasa.gov/FTP',
+    def __init__(self, url='https://heasarc.gsfc.nasa.gov/FTP',
                  start_key='<a href="', end_key='">', table_key='Parent Directory</a>',
                  progress: Progress = None, context: ssl.SSLContext = None):
         super().__init__(progress)
@@ -479,3 +481,313 @@ class Http(BaseProtocol):
     def __repr__(self):
         return '<{0}: url {1}>'.format(self.__class__.__name__, self._url)
 
+
+class BaseFinder(AbstractContextManager, ABC):
+    """A base class for the interface to the HEASARC FTP archive.
+
+    Note:
+        This class should not be directly instantiated, but rather inherited.
+        The inherited class should define a method called
+        ``_construct_path()`` that accepts ``*args``, which are the user-defined
+        parameters required to define the data path, and the method should
+        return the data path as a string
+
+    Parameters:
+        args: The set of parameters needed to define the data path
+        protocol (str, optional): The connection protocol. Default is HTTPS.
+        **kwargs: keyword arguments passed to protocol constructor
+    """
+    def __init__(self, *args, protocol='HTTPS', **kwargs):
+       self.protocol = protocol
+       if protocol in ['HTTP', 'HTTPS']:
+           self._protocol = Http(**kwargs)
+       elif protocol == 'FTP':
+           self._protocol = Ftp(**kwargs)
+       else:
+           raise ValueError("Unrecognized connection protocol " + protocol)
+       self._args = None
+
+       self.cd(*args)
+
+    @property
+    def files(self):
+        """(list of str): The list of files in the current directory"""
+        return self._protocol._file_list
+
+    @property
+    def num_files(self):
+        """(int): Number of files in the current directory"""
+        return self._protocol._num_files
+
+    def cd(self, *args):
+        path = self._construct_path(*args)
+        self._protocol.cd(path)
+
+    def ls(self, *args):
+        path = self._construct_path(*args)
+        return self._protocol.ls(path)
+
+    def filter(self, filetype, extension):
+        """Filters the directory for the requested filetype and extension
+
+        Args:
+            filetype (str): The type of file, e.g. 'cspec'
+            extension (str): The file extension, e.g. '.pha'
+
+        Returns:
+            (list)
+        """
+        return self._file_filter(self.files, filetype, extension)
+
+    def _file_filter(self, file_list, filetype, extension):
+        """Filters the directory for the requested filetype and extension
+
+        Args:
+            file_list (list): A list of files
+            filetype (str): The type of file, e.g. 'cspec'
+            extension (str): The file extension, e.g. '.pha'
+
+        Returns:
+            list: The filtered file list
+        """
+        files = [f for f in file_list if
+                 (filetype in f) & (f.endswith(extension))]
+
+        return files
+
+    @abstractmethod
+    def _construct_path(self, *args) -> str:
+        """This method needs to be defined by the inheriting class.  The method
+        shall accept all user-defined parameters that are required to define 
+        the data path and shall return the data path as a string.
+
+        Args:
+            args: The set of parameters needed to define the data path
+
+        Returns:
+            (str)
+        """
+        pass
+
+    def __exit__(self, __exc_type: Optional[Type[BaseException]], __exc_value: Optional[BaseException],
+                 __traceback: Optional[TracebackType]) -> Optional[bool]:
+        pass
+
+
+class FtpFinder(BaseFinder):
+    def __init__(self, *args, host='heasarc.gsfc.nasa.gov', progress: Progress = None):
+        super().__init__(*args, method='FTP', host=host, progress=progress)
+
+
+class FileDownloader(ProgressMixin):
+    """Used to download a list of files given as a URL."""
+
+    def __init__(self):
+        super().__init__(progress=self._create_progress())
+        self._progress.start()
+        self._http = Http(progress=self._progress)
+        self._ftp = Ftp(host=None, progress=self._progress)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._ftp.disconnect()
+        self._progress.stop()
+
+    def download_url(self, url: str, dest_dir: Union[str, Path], verbose: bool = True):
+        """Download a file from a URL."""
+
+        if url.startswith('ftp'):
+            self._ftp.download_url(url, dest_dir, verbose)
+        elif url.startswith('http'):
+            self._http.download_url(url, dest_dir, verbose)
+        else:
+            raise ValueError('url must begin with ftp://, http://, or https://')
+
+    def bulk_download(self, urls: List[str], dest_dir: Union[str, Path], verbose: bool = True):
+        """Download files from a list of URLs."""
+        for url in urls:
+            self.download_url(url, dest_dir, verbose)
+
+
+
+class BrowseCatalog:
+    """A class that interfaces with the HEASARC Browse API.  This can be
+    called directly, but is primarily intended as a base class.
+
+    This class makes a query to HEASARC's w3query.pl perl script in
+    BATCHRETRIEVALCATALOG mode.  All fields and rows are retrieved so that
+    this class, on instantiation, contains the full set of catalog data.
+    Any queries based on row or columns selections/slices are then done locally,
+    instead of making repeated requests to the HEASARC. A cached copy of the
+    catalog is saved locally so future instantiations can utilize the local copy
+    instead of querying HEASARC.
+
+    Parameters:
+        cache_path (str): The path where the cached catalog will live.
+        cached (bool, optional): Set to True to read from the cached file
+                                 instead of querying HEASARC. Default is False.
+        table (str, optional): The name of the table to be passed to the 
+                               w3query.pl script.
+        verbose (bool, optional): Default is True
+    """
+
+    def __init__(self, cache_path, table=None, verbose=True, cached=False):
+
+        if not os.path.exists(cache_path):
+            os.makedirs(cache_path)
+        self._cache_path = cache_path
+
+        self._table = str(table)
+        self._cached = bool(cached)
+        self._verbose = bool(verbose)
+
+        # just load the cached file
+        if self._cached:
+            self._header, self._data = self._read_cache()
+            return
+
+        # build the URL query
+        host = 'https://heasarc.gsfc.nasa.gov'
+        script = 'db-perl/W3Browse/w3query.pl'
+        query = 'tablehead=name=BATCHRETRIEVALCATALOG_2.0+'
+        # Retrieve all fields, all rows, and return in FITS format
+        query += '{}&Fields=All&displaymode=FitsDisplay&ResultMax=0'.format(table)
+
+        if self._table is not None:
+            self._is_connected(host)
+            self._download_table(host + '/' + script + '?' + query)
+            self._header, self._data = self._read_cache()
+
+    @property
+    def columns(self):
+        """(np.array): The names of the columns available in the table"""
+        return self._data.dtype.names
+
+    @property
+    def num_cols(self):
+        """(int): The total number of columns (fields) in the data table"""
+        return len(self.columns)
+
+    @property
+    def num_rows(self):
+        """(int): The total number of rows in the data table"""
+        return self._data.size
+
+    def column_range(self, column):
+        """Return the data range for a given column, in the form of (low, high).
+
+        Args:
+            column (str): The column name
+
+        Returns:
+            (tuple)
+        """
+        col_copy = np.copy(self._data[column])
+        col_copy.sort()
+        return col_copy[0], col_copy[-1]
+
+    def get_table(self, columns=None):
+        """Return the table data as a numpy record array.
+
+        Args:
+            columns (list of str, optional): The columns to return. If omitted,
+                                             returns all columns.
+
+        Returns:
+            (np.recarray)
+        """
+        if columns is None:
+            columns = self.columns
+
+        arrays = [self._data[col] for col in columns]
+        return np.rec.fromarrays(arrays, names=','.join(columns).upper())
+
+    def slice(self, column, lo=None, hi=None):
+        """Perform row slices of the data table based on a conditional of a
+        single column. Returns a new BrowseCatalog object.
+
+        Args:
+            column (str): The column name
+            lo (optional): The minimum (inclusive) value of the slice. If not
+                           set, uses the lowest range of the data in the column.
+            hi (optional): The maximum (inclusive) value of the slice. If not
+                           set, uses the highest range of the data in the column.
+
+        Returns:
+            (:class:`BrowseCatalog`)
+        """
+        col = self._data[column]
+        if lo is None:
+            lo, _ = self.column_range(column)
+        if hi is None:
+            _, hi = self.column_range(column)
+        mask = (col >= lo) & (col <= hi)
+
+        # create a new object and fill it with the sliced data
+        obj = type(self)(self._cache_path, cached=True)
+        obj._table = self._table
+        obj._header = np.copy(self._header)
+        obj._data = self._data[mask]
+        return obj
+
+    def slices(self, columns):
+        """Perform row slices of the data table based on a conditional of
+        multiple columns. Returns a new BrowseCatalog object.
+
+        Args:
+            columns (list of tuples):
+                A list of tuples, where each tuple is (column, lo, hi).  The
+                'column' is the column name, 'lo' is the lowest bounding value,
+                and 'hi' is the highest bouding value.  If no low or high
+                bounding is desired, set to None. See :meth:`slice()` for more
+                info.
+
+        Returns:
+            (:class:`BrowseCatalog`)
+        """
+        numcols = len(columns)
+        obj = self
+        for i in range(numcols):
+            obj = obj.slice(columns[i][0], lo=columns[i][1], hi=columns[i][2])
+        return obj
+
+    def _download_table(self, url):
+        """Downloads the table from HEASARC
+        """
+        # secure connection
+        if self._verbose:
+            print('Sending request and awaiting response from HEASARC...')
+        t0 = time.time()
+
+        context = ssl._create_unverified_context()
+        page = urlopen(url, context=context)
+
+        if self._verbose:
+            print(f'Downloading {self._table} from HEASARC via w3query.pl...')
+
+        with open(os.path.join(self._cache_path, self._table + '.fit'),
+                  'wb') as f:
+            f.write(page.read())
+
+        if self._verbose:
+            print('Finished in {} s'.format(int(time.time() - t0)))
+
+    def _is_connected(self, host):
+        """Test the connection to the host to determine if it is reachable"""
+        try:
+            sock = socket.create_connection((host.split('/')[-1], 80))
+            sock.close()
+        except OSError:
+            raise OSError("Either you are not connected to the internet or "
+                          "{0} is down.".format(host))
+        return True
+
+    def _read_cache(self):
+        """Read the cached catalog FITS file"""
+        out_file = os.path.join(self._cache_path, self._table + '.fit')
+        with fits.open(out_file) as hdulist:
+            return hdulist[1].header, hdulist[1].data
+
+    def __repr__(self):
+        return '<{0}: {1} columns, {2} rows>'.format(self.__class__.__name__,
+                                                     self.num_cols,
+                                                     self.num_rows)
