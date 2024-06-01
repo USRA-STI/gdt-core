d8da2d02af2cab40f105da148b50e75bee35e805
Suman Bala <sumanbala2210@gmail.com>
2024-06-01 14:39:04 -0500

    Added test case to show 'nan' el value (updated)

--- tests/core/coords/test_spacecraft.py
+++ tests/core/coords/test_spacecraft.py
@@ -35,11 +35,8 @@ from astropy.time import Time
 from gdt.core.coords import SpacecraftFrame, SpacecraftAxes, Quaternion
 from gdt.core.coords.spacecraft.axes import SpacecraftAxesAttribute
 
-import astropy.units as u
-import astropy.coordinates as a_coords
 from astropy.coordinates import FunctionTransform, ICRS, frame_transform_graph
-from gdt.core.coords.spacecraft import SpacecraftFrame
-from gdt.core.coords.spacecraft.frame import spacecraft_to_icrs, icrs_to_spacecraft
+from gdt.core.coords.spacecraft.frame import icrs_to_spacecraft
 
 
 class TestSpacecraftAxes(unittest.TestCase):
@@ -414,7 +411,7 @@ class test_frame_transfer(unittest.TestCase):
             pass
         quaternion_test = [-0.5636783622078214, 0.35243503385029756,
                             -0.34740741282879534, 0.6613352708008627]
-        #quaternion_test = [-0.563678, 0.352435,-0.347407, 0.661335]
+        
         self.sc_frame = testFrame(quaternion=quaternion_test)
         self.z_point = SkyCoord(30.271184, 6.667796, unit='deg')
 
