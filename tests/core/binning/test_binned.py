82653bf55909c50e7776083dcbc1b723740637e7
Adam Goldstein <129613567+AdamGoldstein-USRA@users.noreply.github.com>
2025-09-20 18:05:40 -0500

    Update the prebinned binning algorithms to account for user-specified count uncertainties

--- tests/core/binning/test_binned.py
+++ tests/core/binning/test_binned.py
@@ -28,6 +28,7 @@ from gdt.core.binning.binned import *
 
 
 counts = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0])
+count_uncert = np.sqrt(counts)
 exposure = np.array([1.024, 1.01, 1.00, 0.99, 1.02, 1.024, 0.80, 1.01])
 edges = np.linspace(0.0, 8.192, 9)
 index = np.array([0,2,4,6,8])
@@ -36,54 +37,62 @@ times = np.array([0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 1.0, 1.01, 1.02, 1.03, 1.04
     
 class TestRebinningBinned(unittest.TestCase):
     
-    def test_combine_by_factor(self): 
-        new_counts, new_exposure, new_edges = combine_by_factor(counts, exposure, edges, 4)
+    def test_combine_by_factor(self):
+        res = combine_by_factor(counts, count_uncert, exposure, edges, 4)
+        new_counts, new_uncert, new_exposure, new_edges = res
         self.assertCountEqual(new_counts, np.array([10.0, 26.0]))
         self.assertCountEqual(new_exposure, np.array([4.024, 3.854]))
         self.assertCountEqual(new_edges, np.array([0.0, 4.096, 8.192]))
+        self.assertCountEqual(new_uncert, np.sqrt([10.0, 26.0]))
         
-        self.assertRaises(AssertionError, combine_by_factor, counts, exposure, edges, 0)
+        with self.assertRaises(AssertionError):
+            combine_by_factor(counts, count_uncert, exposure, edges, 0)
 
     def test_rebin_by_time(self): 
-        new_counts, new_exposure, new_edges = rebin_by_time(counts, exposure, edges, 4.096)
+        res = rebin_by_time(counts, count_uncert, exposure, edges, 4.096)
+        new_counts, new_uncert, new_exposure, new_edges = res
         self.assertCountEqual(new_counts, np.array([10.0, 26.0]))
         self.assertCountEqual(new_exposure, np.array([4.024, 3.854]))
         self.assertCountEqual(new_edges, np.array([0.0, 4.096, 8.192]))
-        
-        self.assertRaises(AssertionError, rebin_by_time, counts, exposure, edges, -1.0)
+        self.assertCountEqual(new_uncert, np.sqrt([10.0, 26.0]))
 
+        with self.assertRaises(AssertionError):
+            rebin_by_time(counts, count_uncert, exposure, edges, -1.0)
+        
     def test_combine_into_one(self): 
-        new_counts, new_exposure, new_edges = combine_into_one(counts, exposure, edges)
+        res = combine_into_one(counts, count_uncert, exposure, edges)
+        new_counts, new_uncert, new_exposure, new_edges = res
         self.assertCountEqual(new_counts, np.array([36.0]))
         self.assertCountEqual(new_exposure, np.array([7.878]))
         self.assertCountEqual(new_edges, np.array([0.0, 8.192]))
+        self.assertCountEqual(new_uncert, np.sqrt([36.0]))
     
     def test_rebin_by_edge_index(self):
-        new_counts, new_exposure, new_edges =rebin_by_edge_index(counts, exposure, edges, index)
+        res = rebin_by_edge_index(counts, count_uncert, exposure, edges, index)
+        new_counts, new_uncert, new_exposure, new_edges = res
         self.assertCountEqual(new_counts, np.array([3.0, 7.0, 11.0, 15.0]))
         self.assertCountEqual(new_exposure, np.array([2.034, 1.99, 2.044, 1.81]))
         self.assertCountEqual(new_edges, np.array([0.0, 2.048, 4.096, 6.144, 8.192]))
+        assert np.all(new_uncert.round(6) == np.sqrt([3.0, 7.0, 11.0, 15.0]).round(6))
         
     def test_rebin_by_edge(self):
         new_edges = np.array([0.0, 2.048, 4.096, 6.144, 8.192])
-        new_counts, new_exposure, new_edges =rebin_by_edge(counts, exposure, edges, new_edges)
+        res = rebin_by_edge(counts, count_uncert, exposure, edges, new_edges)
+        new_counts, new_uncert, new_exposure, new_edges = res
         self.assertCountEqual(new_counts, np.array([3.0, 7.0, 11.0, 15.0]))
         self.assertCountEqual(new_exposure, np.array([2.034, 1.99, 2.044, 1.81]))
         self.assertCountEqual(new_edges, np.array([0.0, 2.048, 4.096, 6.144, 8.192]))
+        assert np.all(new_uncert.round(6) == np.sqrt([3.0, 7.0, 11.0, 15.0]).round(6))
 
     def test_rebin_by_snr(self):
         back_counts = np.ones_like(counts)
-        new_counts, new_exposure, new_edges = rebin_by_snr(counts, exposure, edges,
-                                                           back_counts, 3.0)
+        res =  rebin_by_snr(counts, count_uncert, exposure, edges, back_counts, 3.0)
+        new_counts, new_uncert, new_exposure, new_edges = res
         self.assertEqual(new_counts.size, 5)
         self.assertCountEqual(new_counts, np.array([10.0, 5.0, 6.0, 7.0, 8.0]))
         self.assertCountEqual(new_exposure, np.array([4.024, 1.02, 1.024, 0.8, 1.01]))
         self.assertCountEqual(new_edges, np.array([0.0, 4.096, 5.120, 6.144, 7.168, 8.192]))
+        self.assertCountEqual(new_uncert, np.sqrt([10.0, 5.0, 6.0, 7.0, 8.0]))
 
-        self.assertRaises(ValueError, rebin_by_snr, counts, exposure, edges, 
-                          -1.0*back_counts, 3.0)
-
-
-if __name__ == '__main__':
-    unittest.main()
-      
+        with self.assertRaises(ValueError):
+            rebin_by_snr(counts, count_uncert, exposure, edges, -1.0*back_counts, 3.0)
