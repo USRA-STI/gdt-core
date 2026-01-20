f8744d88ffc46c280340da385a969b85e44d17e5
Josh Wood <joshua.randall.wood@gmail.com>
2026-01-20 10:27:08 -0600

    use common mixin for specturm tests

--- tests/core/plot/test_spectrum.py
+++ tests/core/plot/test_spectrum.py
@@ -22,13 +22,8 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-import os
-import numpy as np
-import healpy as hp
 import unittest
 import matplotlib.pyplot as plt
-import astropy.coordinates.representation as r
-import astropy.units as u
 
 from gdt.core.plot.lightcurve import Lightcurve
 from gdt.core.data_primitives import TimeEnergyBins, Gti, ChannelBins
@@ -37,47 +32,36 @@ from gdt.core.background.fitter import BackgroundFitter
 from gdt.core.background.binned import Polynomial
 from gdt.core.plot.spectrum import Spectrum
 
-this_dir = os.path.dirname(__file__)
+from . import MyMixin
 
 
-class TestLightcurve(unittest.TestCase):
+class TestLightcurve(MyMixin, unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.image_file = os.path.join(this_dir, "test.png")
+    counts = [[ 0,  0,  2,  1,  2,  0,  0,  0],
+              [ 3, 16, 10, 13, 14,  4,  3,  3],
+              [ 3, 23, 26, 13,  8,  8,  5,  5],
+              [ 4, 21, 19, 16, 13,  2,  3,  4],
+              [ 4, 20, 17, 11, 15,  2,  1,  5],
+              [ 6, 20, 19, 11, 11,  1,  4,  4]]
+    tstart = [0.0000, 0.0039, 0.0640, 0.1280, 0.1920, 0.2560]
+    tstop = [0.0039, 0.0640, 0.1280, 0.1920, 0.2560, 0.320]
+    exposure = [0.0038, 0.0598, 0.0638, 0.0638, 0.0638, 0.0638]
+    emin = [4.323754, 11.464164, 26.22962, 49.60019, 101.016815,
+            290.46063, 538.1436, 997.2431]
+    emax = [11.464164, 26.22962, 49.60019, 101.016815, 290.46063,
+            538.1436, 997.2431, 2000.]
 
-        counts = [[ 0,  0,  2,  1,  2,  0,  0,  0],
-                  [ 3, 16, 10, 13, 14,  4,  3,  3],
-                  [ 3, 23, 26, 13,  8,  8,  5,  5],
-                  [ 4, 21, 19, 16, 13,  2,  3,  4],
-                  [ 4, 20, 17, 11, 15,  2,  1,  5],
-                  [ 6, 20, 19, 11, 11,  1,  4,  4]]
-        tstart = [0.0000, 0.0039, 0.0640, 0.1280, 0.1920, 0.2560]
-        tstop = [0.0039, 0.0640, 0.1280, 0.1920, 0.2560, 0.320]
-        exposure = [0.0038, 0.0598, 0.0638, 0.0638, 0.0638, 0.0638]
-        emin = [4.323754, 11.464164, 26.22962, 49.60019, 101.016815,
-                290.46063, 538.1436, 997.2431]
-        emax = [11.464164, 26.22962, 49.60019, 101.016815, 290.46063,
-                538.1436, 997.2431, 2000.]
+    data = TimeEnergyBins(counts, tstart, tstop, exposure, emin, emax)
+    gti = Gti.from_list([(0.0000, 0.320)])
+    phaii = Phaii.from_data(data, gti=gti, trigger_time=356223561.133346)
 
-        data = TimeEnergyBins(counts, tstart, tstop, exposure, emin, emax)
-        gti = Gti.from_list([(0.0000, 0.320)])
-        phaii = Phaii.from_data(data, gti=gti, trigger_time=356223561.133346)
+    fitter = BackgroundFitter.from_phaii(phaii, Polynomial)
+    fitter.fit(order=1)
 
-        fitter = BackgroundFitter.from_phaii(phaii, Polynomial)
-        fitter.fit(order=1)
-
-        cls.phaii = phaii
-        cls.spec = phaii.to_spectrum()
-        cls.back_spec = fitter.interpolate_bins(phaii.data.tstart, phaii.data.tstop).integrate_time(0, 0.320)
-        cls.selections = phaii.to_lightcurve(time_range=(0.2, 0.3))
-
-    def tearDown(self):
-        plt.close('all')
-        try:
-            os.remove(self.image_file)
-        except:
-            pass
+    phaii = phaii
+    spec = phaii.to_spectrum()
+    back_spec = fitter.interpolate_bins(phaii.data.tstart, phaii.data.tstop).integrate_time(0, 0.320)
+    selections = phaii.to_lightcurve(time_range=(0.2, 0.3))
 
     def test_spectrum(self):
         s = Spectrum(data=self.spec, background=self.back_spec)
