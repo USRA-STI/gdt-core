6186d5c157e7639eeec2dde4218a10f8e65f46a1
Bill Cleveland <120688282+BillCleveland-USRA@users.noreply.github.com>
2026-02-02 14:38:34 -0600

    Added Astropy copyright and license text to bayesian block module.

--- src/gdt/misc/bayesian_blocks.py
+++ src/gdt/misc/bayesian_blocks.py
@@ -2,6 +2,31 @@ from gdt.core.data_primitives import TimeBins
 
 import numpy as np
 
+# Copyright (c) 2011-2022, Astropy Developers
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted
+# provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this list of conditions
+#    and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+#    and the following disclaimer in the documentation and/or other materials provided with the
+#    distribution.
+#  * Neither the name of the Astropy Team nor the names of its contributors may be used to endorse
+#    or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# From Israel Martinez:
 # Much of this code was copied from the bayesian blocks implementation
 # in astropy https://github.com/astropy/astropy/blob/2db2f820eb51c95fcb3e187328c8cbd99ecd24df/astropy/stats/bayesian_blocks.py
 # With the following differences:
